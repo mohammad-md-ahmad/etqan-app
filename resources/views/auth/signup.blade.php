25caf49906d6149ee66ccb78d34cc2aef65b0503
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-09-22 11:48:04 +0600

    importing js files dynamically

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,51 +1,38 @@
 @extends('layouts/master')
-@section('title', 'signup')
+@section('title', trans('signup'))
 @section('content')
-<h1>Sign Up</h1>
-<form id="signup-form">
-    <div class="py-4">
-        <label>First Name</label>
-        <input type="text" name="first_name" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <label>Last Name</label>
-        <input type="text" name="last_name" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <label>Username</label>
-        <input type="text" name="username" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <label>Email</label>
-        <input type="email" name="email" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <label>Password</label>
-        <input type="password" name="password" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <label>Password Confirmation</label>
-        <input type="text" name="password_confirmation" class="p-2 border block"/>
-    </div>
-    <div class="py-4">
-        <button type="submit" id="submit" class="bg-blue-500 uppercase p-4 px-[25px] mx-2 text-lg font-bold">Submit</button>
-    </div>
-</form>
-<script type="module">
-    $(document).ready(function() {
-        $('#submit').on('click', function (e) {
-            e.preventDefault();
-
-            $.ajax({
-                url: '/auth/signup',
-                type: 'POST',
-                datatype: 'json',
-                data: $('#signup-form').serialize(),
-                success: function (response) {
-
-                }
-            });
-        })
-    })
-</script>
+<div class="mx-auto max-w-[250px] md:max-w-[600px]">
+    <h1 class="text-blue-500 font-bold text-4xl">{{ trans('Sign Up') }}</h1>
+    <form id="signup-form">
+        <div id="error-messages-container" class="m-4 p-4 ps-0 text-red-700"></div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('First Name') }}</label>
+            <input type="text" name="first_name" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('Last Name') }}</label>
+            <input type="text" name="last_name" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('Username') }}</label>
+            <input type="text" name="username" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('Email') }}</label>
+            <input type="email" name="email" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('Password') }}</label>
+            <input type="password" name="password" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="my-4 p-4 border rounded-lg">
+            <label class="font-bold text-blue-500">{{ trans('Password Confirmation') }}</label>
+            <input type="password" name="password_confirmation" class="p-2 border-2 block w-full rounded-lg"/>
+        </div>
+        <div class="flex justify-end my-4 p-4 pe-0">
+            <button type="submit" id="submit" class="bg-blue-500 uppercase p-4 m-0 px-[25px] text-lg font-bold text-white">{{ trans('Submit') }}</button>
+        </div>
+    </form>
+</div>
+@vite('resources/js/auth/signup.js')
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fg from 'fast-glob';
+import path from 'path';
+
+function getEntries(dir) {
+    const files = fg.sync(`${dir}/**/*.js`);
+    return files.reduce((entries, file) => {
+        const key = path.relative(dir, file).replace(/\.js$/, '');
+        entries[key] = path.resolve(file);
+        return entries;
+    }, {});
+}
 
 export default defineConfig({
     plugins: [
@@ -12,5 +23,10 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 3001,
     },
-    publicDir: 'public/assets'
+    publicDir: 'public/assets',
+    build: {
+        rollupOptions: {
+            input: getEntries('resources/js')
+        }
+    }
 });
