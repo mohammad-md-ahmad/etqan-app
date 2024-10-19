8322141e57ff33152273d8b7a4a0124cc46d895d
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-10-19 19:34:38 +0600

    introducing nav item component

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,16 +1,22 @@
-<nav>
-    <a href="{{ route('home') }}">{{ trans(config('app.name')) }}</a>
-    <ul>
+<nav class="flex justify-between items-center px-10 py-2 text-xl bg-blue-500 font-bold">
+    <a href="{{ route('home') }}" class="text-white py-3 px-5">{{ trans(config('app.name')) }}</a>
+    <div>
+        <form method="GET" action="{{ route('search.view') }}">
+            <div>
+                <input type="search" name="searchTerm"/>
+            </div>
+            <div>
+                <button>{{ trans('Submit') }}</button>
+            </div>
+        </form>
+    </div>
+    <ul class="flex justify-end space-x-4 min-w-[200px] text-white">
         @if (! auth()->check())
-        <li>
-            <a href="{{ route('auth.signup.view') }}">{{ trans('sing up') }}</a>
-        </li>
-        <li>
-            <a href="{{ route('auth.login.view') }}">{{ trans('login') }}</a>
-        </li>
+        <x-nav-item href="{{ route('auth.signup.view') }}" active="{{ request()->routeIs('auth.signup.view') }}">{{ trans('sing up') }}</x-nav-item>
+        <x-nav-item href="{{ route('auth.login.view') }}" active="{{ request()->routeIs('auth.login.view') }}">{{ trans('login') }}</x-nav-item>
         @else
-        <li>
-            <a href="{{ route('auth.logout') }}" id="logout-link">{{ trans('logout') }}</a>
+        <li class="text-end flex items-center justify-center">
+            <a href="{{ route('auth.logout') }}" id="logout-link" class="whitespace-nowrap py-3 px-5 rounded-md bg-blue-500">{{ trans('logout') }}</a>
         </li>
         @endif
     </ul>
