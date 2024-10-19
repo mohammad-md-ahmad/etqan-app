7d106e9890a575b69dda8dfcd9aefeed67f7c4fa
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-10-19 19:33:19 +0600

    using scss instead of css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>{{ config('app.name') }} - @yield('title')</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.scss', 'resources/js/app.js'])
     </head>
-    <body class="h-[100vh]">
-        <header class="h-[50px] w-full">
+    <body class="h-full">
+        <header class="w-full">
             <x-nav/>
         </header>
-        <main class="pt-[70px] w-full h-full">
+        <main class="pt-[70px] w-full h-full px-10">
             <div class="">
                 @yield('content')
             </div>
