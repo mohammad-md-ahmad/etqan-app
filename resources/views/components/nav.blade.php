d3d90fc9311cb1c31f01aca0fda74f158dd3f1fb
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-10-27 15:45:53 +0600

    hiding search form for guests

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,10 +1,11 @@
 <nav class="flex justify-between items-center px-10 py-2 text-xl bg-blue-500 font-bold">
     <a href="{{ route('home') }}" class="text-white py-3 px-5">{{ trans(config('app.name')) }}</a>
+    @if (auth()->check())
     <div>
         <form method="GET" action="{{ route('search.view') }}">
             <div class="flex justify-between items-center">
                 <div>
-                    <input type="search" name="searchTerm"/>
+                    <input type="search" name="term"/>
                 </div>
                 <div>
                     <button class="text-white ms-3">{{ trans('Search') }}</button>
@@ -12,6 +13,7 @@
             </div>
         </form>
     </div>
+    @endif
     <ul class="flex justify-end space-x-4 min-w-[200px] text-white">
         @if (! auth()->check())
         <x-nav-item href="{{ route('auth.signup.view') }}" active="{{ request()->routeIs('auth.signup.view') }}">{{ trans('sing up') }}</x-nav-item>
