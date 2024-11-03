df61e8388f55567578a00a992e636c120e2b38c1
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-11-03 18:41:52 +0600

    subscription to the new follower broadcast channel

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,3 +5,25 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import $ from "jquery";
 window.$ = $;
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+// import './echo';
+
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
+
+const pusher = new Pusher('686ebbee59eadcde1c0d', {
+    cluster: 'ap2',
+});
+
+const newFollowerChannel = pusher.subscribe('new-follower');
+
+newFollowerChannel.bind('new-follower-event', function (e) {
+    console.log('new-follower e', e);
+    alert(e?.message);
+});
